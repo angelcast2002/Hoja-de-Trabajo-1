9d30f2bedec582aece1647ee3f8e5c4505528069
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-01-27 18:35:35 -0600

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,5 +1,9 @@
 public class Principal {
 
     // Mora, Azurdia les toca esto jsjsjsjsjs
+    // Acciones por hacer
+    // Hacer el menu, creo que tiene que ser en GUI
+    // Hacer el UML
+    // Implementar JUnit, esto pone la guia al chile no c que es NO deben ser simplemente los tests generados automáticamente por el IDE, recuerde que tiene que modificarlos para que realmente prueben los métodos de su clase
     
 }
