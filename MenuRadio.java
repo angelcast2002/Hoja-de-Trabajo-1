da9880c559a31b39eba66c0c30c28e05a7366ee1
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-01-30 10:18:20 -0600

    main and scan
    Main was created. It runs.
    I changed a bit how I used the scanner class.

--- MenuRadio.java
+++ MenuRadio.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class MenuRadio {
     Acciones act = new Acciones();
+    Scanner scan = new Scanner(System.in);
     public void menu(){
-        show("Bienvenido usuario. ¿Que quieres hacer?\n");
-        show("1. Encender la radio");
-        show("2. Cambiar de AM a FM");
-        show("3. Avanzar en el dial de las emisoras");
-        show("4. Guardar una emisora");
-        show("5. Seleccionar emisora guardada");
-        show("6. Apagar la radio");
-        show("->");
-        
         boolean salir = false;
-        while (salir == false) {
-            Scanner scan = new Scanner(System.in);
-            switch (scan.nextInt()) {
-                
+        while (salir != true) {
+            show("Bienvenido usuario. ¿Que quieres hacer?\n");
+            show("1. Encender la radio");
+            show("2. Cambiar de AM a FM");
+            show("3. Avanzar en el dial de las emisoras");
+            show("4. Guardar una emisora");
+            show("5. Seleccionar emisora guardada");
+            show("6. Apagar la radio");
+            show("7. Terminar la ejecución de programa. ");
+            show("->");
+            
+            int ans = scan.nextInt();
+            switch (ans) {   
                 case 1: // Enciende la radio y devuleve un mensaje que indica el estado de la radio.
                     act.encenderApagar();
                     act.comprobarEncendido();
@@ -32,15 +33,13 @@ public class MenuRadio {
                     show("Introduce:");
                     show("1. para AM");
                     show("2. para FM");
-                    Scanner scan1 = new Scanner(System.in);
                     boolean frec;
-                    if (scan1.nextInt()== 2) {
+                    if (scan.nextInt()== 2) {
                         frec = false; // FM
                     } else{
                         frec = true; // AM
                     }
                     show(act.cambiarSenal(frec));
-                    scan1.close();
                     break;
             
                 case 3: //Sube a la siguiente emisora. 
@@ -54,17 +53,12 @@ public class MenuRadio {
                     show("La emisora actual es: " + act.getEmisoraActual());
                     show("Se guardará con tus emisoras favoritas.");
                     show("Ingresa el botón, entre 1 y 12, en el que quieras guardar tu emisora.");
-                    Scanner scan2 = new Scanner(System.in);
-                    act.guardarEmisoraActual(scan2.nextInt());
-                    scan2.close();
-
+                    act.guardarEmisoraActual(scan.nextInt());
                     break;
 
                 case 5: // selecciona la emisora guardada. 
                     show("Selecciona el botón para establecer una de tus emisoras favoritas. ");
-                    Scanner scan3 = new Scanner(System.in);
-                    act.seleccionarEmisoraGuardad(scan3.nextInt());
-                    scan3.close();
+                    act.seleccionarEmisoraGuardad(scan.nextInt());
                     break;
 
                 case 6: // apaga la radio
--- Principal.java
+++ Principal.java
@@ -5,4 +5,12 @@ public class Principal {
     // Hacer el menu, creo que tiene que ser en GUI
     // Hacer el UML
     // Implementar JUnit, esto pone la guia al chile no c que es NO deben ser simplemente los tests generados automáticamente por el IDE, recuerde que tiene que modificarlos para que realmente prueben los métodos de su clase
+
+    public static void main (String[] args){
+        MenuRadio menu = new MenuRadio();
+        menu.menu();
+    }
+
+
+
 }
