b8a7796ab7981c7bd3d124d6602aad2e86005f0a
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2022-01-30 01:02:50 -0600

    First draft
    update. waiting for approval. Changes may be done.

--- MenuRadio.java
+++ MenuRadio.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuRadio {
@@ -12,56 +11,75 @@ public class MenuRadio {
         show("5. Seleccionar emisora guardada");
         show("6. Apagar la radio");
         show("->");
-        Scanner scanner = new Scanner(System.in);
-        switch (scanner.nextInt()) {
-            
-            case 1: // Enciende la radio y devuleve un mensaje que indica el estado de la radio.
-                act.encenderApagar();
-                act.comprobarEncendido();
-                if (act.comprobarEncendido() == true) {
-                    show("La radio está encendida");
-                } else {
-                    show("La radio no está encendida");
-                }
-
-                break;
-            
-            case 2:
-                show("Introduce:");
-                show("1. para AM");
-                show("2. para FM");
-                Scanner scan = new Scanner(System.in);
-                boolean frec;
-                if (scan.nextInt()== 2) {
-                    frec = false; // FM
-                } else{
-                    frec = true; // AM
-                }
-                show(act.cambiarSenal(frec));
-                break;
         
-            case 3:
-                act.subirEmisora();
-                show("La emisora actual es:");
-                show(act.getEmisoraAguardar());
-
-                break;
-
-            case 4:
+        boolean salir = false;
+        while (salir == false) {
+            Scanner scan = new Scanner(System.in);
+            switch (scan.nextInt()) {
                 
+                case 1: // Enciende la radio y devuleve un mensaje que indica el estado de la radio.
+                    act.encenderApagar();
+                    act.comprobarEncendido();
+                    if (act.comprobarEncendido() == true) {
+                        show("La radio está encendida");
+                    } else {
+                        show("La radio no está encendida");
+                    }
+                    
+                    break;
+                
+                case 2: // Selecciona AM o FM
+                    show("Introduce:");
+                    show("1. para AM");
+                    show("2. para FM");
+                    Scanner scan1 = new Scanner(System.in);
+                    boolean frec;
+                    if (scan1.nextInt()== 2) {
+                        frec = false; // FM
+                    } else{
+                        frec = true; // AM
+                    }
+                    show(act.cambiarSenal(frec));
+                    scan1.close();
+                    break;
+            
+                case 3: //Sube a la siguiente emisora. 
+                    act.subirEmisora();
+                    show("La emisora actual es:");
+                    show(act.getEmisoraAguardar());
 
+                    break;
 
+                case 4: //Guarda la emisora actual
+                    show("La emisora actual es: " + act.getEmisoraActual());
+                    show("Se guardará con tus emisoras favoritas.");
+                    show("Ingresa el botón, entre 1 y 12, en el que quieras guardar tu emisora.");
+                    Scanner scan2 = new Scanner(System.in);
+                    act.guardarEmisoraActual(scan2.nextInt());
+                    scan2.close();
 
+                    break;
 
+                case 5: // selecciona la emisora guardada. 
+                    show("Selecciona el botón para establecer una de tus emisoras favoritas. ");
+                    Scanner scan3 = new Scanner(System.in);
+                    act.seleccionarEmisoraGuardad(scan3.nextInt());
+                    scan3.close();
+                    break;
 
+                case 6: // apaga la radio
+                    show("Apagando la radio");
+                    act.encenderApagar();
+                    break;
+
+                case 7: // Sale del ciclo. 
+                salir = true;
+
+            }
         }
-
-
-
-
     }
     
-    public void show(String string){
+    public void show(String string){ // sustituye System.out.println("something");
         System.out.println(string);
     }
 
